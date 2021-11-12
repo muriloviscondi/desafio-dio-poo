9e42d9b48c03d7ca475bb6a2f8d55ac1f5d54d89
Murilo Viscondi <50927186+muriloviscondi@users.noreply.github.com>
2021-11-12 06:37:21 -0300

    Mnetoria e Curso

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("descrição curso java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso js");
+        curso2.setDescricao("descrição curso js");
+        curso2.setCargaHoraria(4);
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("mentoria de java");
+        mentoria1.setDescricao("descrição mentoria java");
+        mentoria1.setData(LocalDate.now());
+
+        Mentoria mentoria2 = new Mentoria();
+        mentoria1.setTitulo("mentoria de js");
+        mentoria1.setDescricao("descrição mentoria js");
+        mentoria1.setData(LocalDate.now());
+
+        System.out.println(mentoria1);
+        System.out.println(mentoria2);
+    }
+}
